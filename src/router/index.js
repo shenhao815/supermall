8f783f2cfdfc5c9cd6f92a4e0b4657b5b553ec6d
shenhao <391531617@qq.com>
2020-05-05 12:37:30 +0800

    ''

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 1.安装插件
+Vue.use(VueRouter)
+
+// 2.创建router
+const routes = []
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+export default router
